1b64efa3c55ed4dc6f8f5b9ac738659a85c72e5c
Yannick fumukani <slashcs7@hotmail.com>
2022-10-05 16:35:04 -0500

    Uniqueness of word implemented

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-
-import java.io.IOException;
-
-
 public class Main {
 
     public static void main(String[] args) {
 
         var mobydick = new WordProcessor("mobydick.txt");
 
-        mobydick.displayArray();
+       // mobydick.displayArray();
+        mobydick.displayUniquenessResult();
 
 
     }
--- WordProcessor.java
+++ WordProcessor.java
@@ -1,19 +1,20 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WordProcessor {
 
-    private  String[] allWords;
+    private String[] allWords;
     private String fileContent;
-    private String originalText;
-    private Scanner fileScanner;
+    private HashMap<String, Integer> uniqueWords;
 
     public WordProcessor(String filePath) {
 
         try {
-            fileScanner = new Scanner(new File(filePath));
-          //  originalText = fileScanner.
+            Scanner fileScanner = new Scanner(new File(filePath));
+            uniqueWords = new HashMap<String, Integer>();
             fileContent = fileScanner.useDelimiter("//A").next();
             this.processWord();
             fileScanner.close();
@@ -23,15 +24,13 @@ public class WordProcessor {
     }
 
     /**
-     * @Description
-     * iterate through the entire array of words
+     * @Description iterate through the entire array of words
      * and displays on the screen
      * @Param none
      * @Return void
-     *
      */
     public void displayArray() {
-        for(var word: this.allWords) {
+        for (var word : this.allWords) {
             System.out.println(word);
         }
     }
@@ -41,20 +40,19 @@ public class WordProcessor {
     }
 
     /**
-     * @Description
-     * Get rid of all necessary punctuations
+     * @Description Get rid of all necessary punctuations
      * @Param none
      * @Return void
      */
-    private void stripPonctuation () {
-        this.fileContent = this.fileContent.replaceAll("[,:/()”“’;!]", " ");
+    private void stripPonctuation() {
+        // —
+        this.fileContent = this.fileContent.replaceAll("[,:/()”“‘’;!?—]", " ");
         this.fileContent = this.fileContent.replaceAll("\\.\\s+", " ");
         this.fileContent = this.fileContent.replaceAll("\\s+", " ");
     }
 
     /**
-     * @Description
-     * Convert the file content to an array of words
+     * @Description Convert the file content to an array of words
      * @Param none
      * @Return void
      */
@@ -62,16 +60,59 @@ public class WordProcessor {
         this.allWords = this.fileContent.split(" ");
     }
 
+    private void wordUniqueness() {
+        for (var word : this.allWords) {
+            if (this.uniqueWords.containsKey(word)) {
+                this.uniqueWords.put(word, uniqueWords.get(word) + 1);
+            } else {
+                this.uniqueWords.put(word, 1);
+            }
+        }
+    }
+
+
     /**
-     * @Description
-     * process what requires
+     *
+     * @param w:HasMap<String, Integer>
+     */
+    private void displayUniqueWordCount(HashMap<String, Integer> w) {
+        for (var o : w.entrySet()) {
+            System.out.println(o.getKey() + " | " + o.getValue());
+        }
+    }
+
+    /**
+     * @description
+     * Displays the first 100 most occurent words
+     * @Params none
+     * @return none
+     */
+    public void displayFirst100() {
+       // displayUniqueWordCount(this.selectFirst100());
+    }
+
+
+    public void displayUniquenessResult() {
+        this.displayUniqueWordCount(this.uniqueWords);
+    }
+
+
+
+//    public HashMap<String, Integer> selectFirst100() {
+//        return this.uniqueWords.entrySet().stream().map(Map.Entry::getValue).sorted().limit(100);
+//    }
+
+
+    /**
+     * @Description process what requires
      * get rid of all punctuations
      * and convert to array
      * @Param none
      * @Return void
      */
-    public void processWord () {
+    public void processWord() {
         stripPonctuation();
         convertToArray();
+        wordUniqueness();
     }
 }
