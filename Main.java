976d261a6914fb94badd975ec81aaeb0de29fa47
Yannick fumukani <slashcs7@hotmail.com>
2022-10-05 13:06:37 -0500

    Display array string implemented

--- Main.java
+++ Main.java
@@ -1,23 +1,38 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // Array for capturing every word
+        String[] allWords;
+
         try {
+            // reading the file from disc
+            Scanner fileScanner = new Scanner(new File("mobydick.txt"));
+            // Getting all the file content
+            String fileContent = fileScanner.useDelimiter("\\A").next();
+            fileScanner.close();
 
-            BufferedReader br = new BufferedReader(new FileReader("mobydick.txt"));
-            String line = br.readLine();
+            allWords = fileContent.split(" ");
 
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
-            }
+            displayArray(allWords);
+            // Checking file content
+            //System.out.println(fileContent);
 
-        } catch (Exception e) {
-            System.out.println("An error occured: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("An error has occurred: " + e.getMessage());
         }
 
+    }
+
+    public static void displayArray(String[] arr) {
+        for(var word: arr) {
+            System.out.println(word);
+        }
     }
 }
